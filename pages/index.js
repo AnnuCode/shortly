8fd45cf7675d66e3eb31563dc86bc78ee31e0ae7
Annu Singh <annu4444.as@gmail.com>
2022-08-02 14:15:30 +0530

    cleaning files

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,21 @@
-import Head from "next/head";
-import Image from "next/image";
-// import styles from "../styles/Home.module.css";
-import { Container } from "../src/components/styled/Container.styled";
-import Header from "../src/components/Header";
 import { ThemeProvider } from "@emotion/react";
-import { GlobalStyles } from "../src/components/styled/Global";
+
 import Hero from "../src/components/Hero";
-import Panel from "../src/components/URLInputBox";
-import URLInputBox from "../src/components/URLInputBox";
+
 import AdvancedSection from "../src/components/AdvancedSection";
 import Footer from "../src/components/Footer";
-import AltFooter from "../src/components/AltFooter";
+
 import Headerrr from "../src/components/Headerrr";
-import {Global, css} from '@emotion/react'
+import { Global, css } from "@emotion/react";
 
 const styles = css`
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700;800&display=swap');
-body{
-  font-family: 'Poppins', sans-serif;
-}
-  `
-
-  
-
+  @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700;800&display=swap");
+  body {
+    font-family: "Poppins", sans-serif;
+  }
+`;
 
 const theme = {
-  
-  
   colors: {
     header: "#fff",
     body: "#fff",
@@ -34,23 +23,17 @@ const theme = {
     tomato: "red",
     heading: "rgb(31 41 55)",
   },
- 
-  
 };
 
 export default function Home() {
-
-
-
   return (
     <ThemeProvider theme={theme}>
       <>
-        
-      <Global styles={styles} />
-        
-        <Headerrr/>
+        <Global styles={styles} />
+
+        <Headerrr />
         <Hero />
-        
+
         <AdvancedSection />
         <Footer />
       </>
